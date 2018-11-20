78bedf9c997f749727ac607d051e04435f04cf30
Josh Powell <josh@innovativeops.co>
2018-11-20 11:43:08 -0500

    Add LastPass

---
+++ stts/Services/StatusPage/LastPass.swift
@@ -0,0 +1,11 @@
+//
+//  Lob.swift
+//  stts
+//
+
+import Foundation
+
+class Lob: StatusPageService {
+    let url = URL(string: "http://status.lob.com")!
+    let statusPageID = "2xkb3rfdd3lg"
+}
